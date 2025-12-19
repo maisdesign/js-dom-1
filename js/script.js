119c52ca530c8630bc4a00729351cfd85bc7e66a
Maisdesign <downloadtaky@gmail.com>
2025-12-19 19:52:26 +0100

    Esercizio completato, vorrei però migliorarne l'efficienza

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-document.getElementById("onOff").onclick = function() {lightSwitch(document.getElementById("lightFigure").getElementsByTagName("img")[0])};
+document.getElementById("onOff").onclick = function() {
+    lightSwitch(document.getElementById("lightFigure").getElementsByTagName("img")[0]);
+    buttonSwitch(document.getElementById("onOff"));
+};
+/*
+* Un modo alternativo per selezionare l'immagine sarebbe potuto essere: concatenare il nome del file e cambiare solo quello nel tag src
+*/
 function lightSwitch(image) {
     if (image.src.match("img/white_lamp.png")) {
         image.src = "img/yellow_lamp.png";
@@ -6,5 +12,12 @@ function lightSwitch(image) {
     } else {
         image.src = "img/white_lamp.png";
         image.alt = "Luce Spenta";
+    }
+}
+function buttonSwitch(button) {
+    if (button.innerHTML === "Accendi la luce") {
+        button.innerHTML = "Spegni la luce";
+    } else {
+        button.innerHTML = "Accendi la luce";
     }
 }
